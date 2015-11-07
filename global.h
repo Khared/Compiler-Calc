fca841e14ec3f7571433941cecb42e199288855d
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2015-11-07 15:11:32 -0300

    Create global.h

---
+++ global.h
@@ -0,0 +1,41 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include "symtab.h"
+
+#define MAX_CHILDREN 3
+
+enum node_kind {stmt_k, expr_k};
+enum stmt_kind {if_k, while_k, attrib_k, write_k, read_k};
+enum expr_kind {op_k, id_k, const_k};
+
+struct token_t {
+	int lineno;
+	union {
+		int val;
+		char *name;
+	} value;
+};
+
+struct node_t {
+	struct node_t *child[MAX_CHILDREN];
+	struct node_t *next;
+	int lineno;
+	enum node_kind kind;
+
+	union {
+		enum stmt_kind stmt;
+		enum expr_kind expr;
+	} type;
+
+	union {
+		int op;
+		int val;
+		char *name;
+	} attr;
+};
+
+struct node_t *ast;
+struct symtab_t **stab;
+
+#endif /* _GLOBAL_H_ */
